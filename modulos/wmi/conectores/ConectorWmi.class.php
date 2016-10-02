4318b4af9875670cbd3f1912669a4fda74716a84
Fábio Jânio <contato@fabiojanio.com>
2016-10-02 10:38:20 -0300

    pequenos ajustes no método de conexão

--- modulos/wmi/conectores/ConectorWmi.class.php
+++ modulos/wmi/conectores/ConectorWmi.class.php
@@ -34,32 +34,34 @@ class ConectorWmi
      * Método de conexão para máquinas Windows
      * 
      * @access public
-     * @param string $maquina
+     * @param string $host
      * @param string $usuario
      * @param string $senha
+     * @param int $porta
+     * @param int $timeout
      * @return bool|string Conexão com sucesso retorna true, do contrario string de erro
      */
-    public static function conectar($maquina, $usuario = null, $senha = null)
+    public static function conectar($host, $usuario = null, $senha = null, $porta = 135, $timeout = 10)
     {
         try
         {
             /**
              * Método utilizado para testar conectividade com o host alvo
              * 
-             * @param string $maquina
-             * @param string $port
+             * @param string $host
+             * @param string $porta
              * @param int $errno valor de sistema
              * @param string $errstr mensagem de sistema
              * @param int $timeout tempo máximo a esperar por uma tentativa de conexão via socket
              */            
-            if (!@fsockopen($maquina, $port = 135, $errno, $errstr, $timeout = 10))
+            if (!@fsockopen($host, $porta, $errno, $errstr, $timeout))
             {
-                throw new Exception("Erro ({$errno}): Time Out ao chamar o host <b>{$maquina}</b>!");
+                throw new Exception("Erro ({$errno}): Time Out ao chamar o host <b>{$host}</b>!");
             }
 
             $WbemLocator = new COM("WbemScripting.SWbemLocator");
             // @see https://msdn.microsoft.com/en-us/library/aa393720(v=vs.85).aspx
-            self::$conexao = $WbemLocator->ConnectServer($maquina, 'root\cimv2', $usuario, $senha, 'MS_416');
+            self::$conexao = $WbemLocator->ConnectServer($host, 'root\cimv2', $usuario, $senha, 'MS_416');
             self::$conexao->Security_->ImpersonationLevel = 3;
         }
         catch (com_exception $e)
